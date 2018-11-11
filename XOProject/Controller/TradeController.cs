38e12fc099d3d788a177c181cb5222cd6dc76d4a
Gregory Levit <levitg@gmail.com>
2018-11-11 23:10:21 +0300

    Add project files.

---
+++ XOProject/Controller/TradeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace XOProject.Controller
+{
+    [Route("api/Trade/")]
+    public class TradeController : ControllerBase
+    {
+        private IShareRepository _shareRepository { get; set; }
+        private ITradeRepository _tradeRepository { get; set; }
+        private IPortfolioRepository _portfolioRepository { get; set; }
+
+        public TradeController(IShareRepository shareRepository, ITradeRepository tradeRepository, IPortfolioRepository portfolioRepository)
+        {
+            _shareRepository = shareRepository;
+            _tradeRepository = tradeRepository;
+            _portfolioRepository = portfolioRepository;
+        }
+
+
+        [HttpGet("{portFolioid}")]
+        public async Task<IActionResult> GetAllTradings([FromRoute]int portFolioid)
+        {
+            var trades = await _tradeRepository.Query().Where(x => x.PortfolioId.Equals(portFolioid)).ToListAsync();
+            
+            if (trades.Count == 0) {
+                return NotFound();
+            }
+
+            return Ok(trades);
+        }
+
+
+        /// <summary>
+        /// For a given symbol of share, get the statistics for that particular share calculating the maximum, minimum, average and sum of price for all the trades that happened for that share. 
+        /// Group statistics individually for all BUY trades and SELL trades separately.
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+
+        [HttpGet("Analysis/{symbol}")]
+        public async Task<IActionResult> GetAnalysis([FromRoute]string symbol)
+        {
+            var list = new List<TradeAnalysis>();
+
+            return Ok(list);
+        }
+
+
+    }
+}
